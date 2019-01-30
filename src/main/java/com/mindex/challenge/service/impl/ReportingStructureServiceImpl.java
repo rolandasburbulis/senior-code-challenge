c3d167f2e292e3c0b563534ea02b92be6d7848d4
Roland Burbulis <rolandas.burbulis@thomsonreuters.com>
2019-01-30 02:12:19 -0500

    Initial Reporting Structure endpoint implementation with dummy data for number of reports.

---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,37 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureServiceImpl.class);
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public ReportingStructure read(String id) {
+        LOG.debug("Getting reporting structure for employee with id [{}]", id);
+
+        Employee employee = employeeRepository.findByEmployeeId(id);
+
+        if (employee == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        ReportingStructure reportingStructure = new ReportingStructure();
+
+        reportingStructure.setEmployee(employee);
+        reportingStructure.setNumberOfReports(employee.getDirectReports().size() + 17);
+
+        return reportingStructure;
+    }
+}
